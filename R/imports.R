a8bb4be0086fd528f489f8d7d6c2ef34280e94e4
Elham Ebrahimi <eebrahimi.bio@gmail.com>
2026-05-11 00:22:27 +0200

    minor update

---
+++ R/imports.R
@@ -0,0 +1,64 @@
+# Author: Elham Ebrahimi, eebrahimi.bio@gmail.com
+# Last Update: May 2026
+# Licence: GPL v3
+
+# Package-level imports used internally by camtrapReport.
+#
+# After editing this file, run:
+# devtools::document()
+# devtools::load_all()
+
+#' camtrapReport package imports
+#'
+#' Internal package imports used by camtrapReport.
+#'
+#' @importFrom dplyr %>%
+#' @importFrom dplyr across
+#' @importFrom dplyr arrange
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr case_when
+#' @importFrom dplyr coalesce
+#' @importFrom dplyr count
+#' @importFrom dplyr desc
+#' @importFrom dplyr distinct
+#' @importFrom dplyr filter
+#' @importFrom dplyr group_by
+#' @importFrom dplyr if_else
+#' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr n
+#' @importFrom dplyr n_distinct
+#' @importFrom dplyr pull
+#' @importFrom dplyr rename
+#' @importFrom dplyr select
+#' @importFrom dplyr summarise
+#' @importFrom dplyr ungroup
+#' @importFrom rlang .data
+#' @importFrom methods is
+#' @importFrom methods isGeneric
+#' @importFrom methods new
+#' @importFrom methods setGeneric
+#' @importFrom methods setMethod
+#' @importFrom methods setRefClass
+#' @importFrom stats cor
+#' @importFrom stats na.omit
+#' @importFrom stats setNames
+#' @importFrom utils install.packages
+#' @importFrom utils packageVersion
+#' @importFrom utils read.csv
+#' @importFrom utils remove.packages
+#' @importFrom utils unzip
+#' @importFrom graphics image
+#' @importFrom graphics legend
+#' @importFrom graphics par
+#' @importFrom graphics text
+#' @importFrom grDevices colorRampPalette
+#' @importFrom terra buffer
+#' @importFrom terra crop
+#' @importFrom terra crs
+#' @importFrom terra ext
+#' @importFrom terra project
+#' @importFrom terra rast
+#' @importFrom terra vect
+#' @keywords internal
+"_PACKAGE"
