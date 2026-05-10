ecfeef7a192cb762ec0b27869cc992fad64815f9
Elham Ebrahimi <eebrahimi.bio@gmail.com>
2026-05-11 00:50:21 +0200

    update

--- R/imports.R
+++ R/imports.R
@@ -4,9 +4,15 @@
 
 # Package-level imports used internally by camtrapReport.
 #
+# This file is used by roxygen2 to generate NAMESPACE.
 # After editing this file, run:
-# devtools::document()
-# devtools::load_all()
+#
+#   devtools::document()
+#   devtools::load_all()
+#
+# If a function is used without package prefix, e.g. mutate(), left_join(),
+# distinct(), vect(), rast(), etc., it must be imported here or called with
+# its namespace, e.g. dplyr::mutate().
 
 #' camtrapReport package imports
 #'
@@ -15,6 +21,7 @@
 #' @importFrom dplyr %>%
 #' @importFrom dplyr across
 #' @importFrom dplyr arrange
+#' @importFrom dplyr bind_cols
 #' @importFrom dplyr bind_rows
 #' @importFrom dplyr case_when
 #' @importFrom dplyr coalesce
@@ -22,17 +29,30 @@
 #' @importFrom dplyr desc
 #' @importFrom dplyr distinct
 #' @importFrom dplyr filter
+#' @importFrom dplyr full_join
 #' @importFrom dplyr group_by
 #' @importFrom dplyr if_else
+#' @importFrom dplyr inner_join
 #' @importFrom dplyr left_join
 #' @importFrom dplyr mutate
 #' @importFrom dplyr n
 #' @importFrom dplyr n_distinct
 #' @importFrom dplyr pull
 #' @importFrom dplyr rename
+#' @importFrom dplyr right_join
 #' @importFrom dplyr select
+#' @importFrom dplyr semi_join
 #' @importFrom dplyr summarise
+#' @importFrom dplyr summarize
+#' @importFrom dplyr transmute
 #' @importFrom dplyr ungroup
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr pivot_wider
+#' @importFrom tidyr replace_na
+#' @importFrom tidyr separate
+#' @importFrom tidyr unite
+#' @importFrom tibble as_tibble
+#' @importFrom tibble tibble
 #' @importFrom rlang .data
 #' @importFrom methods is
 #' @importFrom methods isGeneric
@@ -40,25 +60,62 @@
 #' @importFrom methods setGeneric
 #' @importFrom methods setMethod
 #' @importFrom methods setRefClass
+#' @importFrom stats aggregate
+#' @importFrom stats approx
+#' @importFrom stats complete.cases
 #' @importFrom stats cor
+#' @importFrom stats density
+#' @importFrom stats dist
+#' @importFrom stats lm
+#' @importFrom stats median
 #' @importFrom stats na.omit
+#' @importFrom stats predict
+#' @importFrom stats quantile
+#' @importFrom stats sd
 #' @importFrom stats setNames
+#' @importFrom stats time
+#' @importFrom utils capture.output
+#' @importFrom utils data
+#' @importFrom utils download.file
 #' @importFrom utils install.packages
 #' @importFrom utils packageVersion
 #' @importFrom utils read.csv
 #' @importFrom utils remove.packages
 #' @importFrom utils unzip
+#' @importFrom utils write.csv
+#' @importFrom graphics abline
+#' @importFrom graphics axis
+#' @importFrom graphics barplot
+#' @importFrom graphics box
+#' @importFrom graphics boxplot
 #' @importFrom graphics image
 #' @importFrom graphics legend
+#' @importFrom graphics lines
+#' @importFrom graphics mtext
 #' @importFrom graphics par
+#' @importFrom graphics plot
+#' @importFrom graphics points
+#' @importFrom graphics polygon
 #' @importFrom graphics text
 #' @importFrom grDevices colorRampPalette
+#' @importFrom grDevices dev.off
+#' @importFrom grDevices hcl.colors
+#' @importFrom grDevices png
+#' @importFrom grDevices rainbow
+#' @importFrom grDevices rgb
+#' @importFrom grDevices terrain.colors
+#' @importFrom terra as.data.frame
 #' @importFrom terra buffer
 #' @importFrom terra crop
+#' @importFrom terra crds
 #' @importFrom terra crs
 #' @importFrom terra ext
+#' @importFrom terra extract
+#' @importFrom terra mask
 #' @importFrom terra project
 #' @importFrom terra rast
+#' @importFrom terra readRDS
 #' @importFrom terra vect
+#' @importFrom terra writeRaster
 #' @keywords internal
 "_PACKAGE"
