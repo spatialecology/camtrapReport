823a3bd98f58b3fa4fdaf4d0791141e032ff17da
spatialecology <eebrahimi.bio@gmail.com>
2026-08-14 12:50:16 +0200

    Suppress intentional lint exceptions

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,7 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
-library(testthat) # nolint: undesirable_function_linter.
-library(camtrapReport) # nolint: undesirable_function_linter.
+library(testthat) # nolint
+library(camtrapReport) # nolint
 
 test_check("camtrapReport")
